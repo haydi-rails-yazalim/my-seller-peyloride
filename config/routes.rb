d6846978cea0d11d57324c7aa4d4a73cca668e1f
Okan Binli <okanb3@gmail.com>
2017-03-18 19:01:57 +0300

    Carts controller added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'teas#index'
+
   get 'teas/:id', to: 'teas#show'
   get 'add_to_cart/:id', to: 'teas#add_to_cart'
+
+  get 'carts/:id', to: 'carts#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
