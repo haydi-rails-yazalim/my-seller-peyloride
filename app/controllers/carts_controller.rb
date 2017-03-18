f8d4c037ee2f2ea3041e137f0f32ea5162ac4ee7
Okan Binli <okanb3@gmail.com>
2017-03-18 19:25:36 +0300

    cart functionality completed

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,10 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show]
   def show
+    @total_cost = 0
+    @cart.teas.each do |tea|
+      @total_cost = @total_cost + tea.price
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'teas#index'
 
-  get 'teas/:id', to: 'teas#show'
+  get 'teas/:id', to: 'teas#show', as: 'my_tea'
   get 'add_to_cart/:id', to: 'teas#add_to_cart'
 
-  get 'carts/:id', to: 'carts#show'
+  get 'carts/:id', to: 'carts#show', as: 'my_cart'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
