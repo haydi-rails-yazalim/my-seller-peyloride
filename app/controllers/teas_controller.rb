9a0309151f5c92c40f88504539a1ffafbd704e09
Okan Binli <okanb3@gmail.com>
2017-03-18 18:11:57 +0300

    haml gem added and teas controller

--- app/controllers/teas_controller.rb
+++ app/controllers/teas_controller.rb
@@ -1,7 +1,15 @@
 class TeasController < ApplicationController
+  before_action :set_tea, only: [:show]
   def index
+    @teas = Tea.all
   end
 
   def show
+  end
+
+  private
+
+  def set_tea
+    @tea = Tea.find(params[:id])
   end
 end
